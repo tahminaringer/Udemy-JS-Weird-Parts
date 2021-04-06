4b2562afecf5e5072c43031ee9abf7d3e3b27ee5
Tahmina <tahmina.ringer@gmail.com>
2021-04-06 12:04:18 -0700

    notes and code for part of section3

--- starterCode/global-envir-obj/B3_GlobalEnvironment/Starter/app.js
+++ starterCode/global-envir-obj/B3_GlobalEnvironment/Starter/app.js
@@ -65,18 +65,59 @@
 
 // Excersize 18
 
-function waitThreeSeconds() {
-  var ms = 3000 + new Date().getTime();
-  while(new Date() < ms){}
-  console.log('finished function');
-}
+// function waitThreeSeconds() {
+//   var ms = 3000 + new Date().getTime();
+//   while(new Date() < ms){}
+//   console.log('finished function');
+// }
 
-function clickHandler(){
-  console.log('click event!');
-}
+// function clickHandler(){
+//   console.log('click event!');
+// }
 
-//listen for the click event
-document.addEventListener('click', clickHandler);
+// //listen for the click event
+// document.addEventListener('click', clickHandler);
 
-waitThreeSeconds();
-console.log('finished execution');
+// waitThreeSeconds();
+// console.log('finished execution');
+
+// Excercize 18
+// var a = 3 + 4;
+// console.log(a);
+
+// // Excersize 22
+
+// right to left
+// var a = 3 + 4 * 5;
+// console.log(a);
+
+//right to left
+// var a = 2, b = 3, c = 4;
+// a = b = c;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Excersize 25
+console.log(1 < 2 < 3); //(1 < 2 = true < 3) (true = 1 < 3) (1 < 3 = true)
+console.log(3 < 2 < 1); //(3 < 2 = false < 1) (false < 1 = true)
+//Number(false) = 0
+//Number(true) = 1
+//Number(null) = 0
+//Number(undefined) = NaN
+
+// var a = 0
+// var b = false;
+
+// if(a == b) {
+//   console.log('they are equal'); //run code get this
+// } else {
+//   console.log('Nope, not equal');
+// }
+
+// if(a === b) {
+//   console.log('they are equal');
+// } else {
+//   console.log('Nope, not equal'); //run code get this
+// }
