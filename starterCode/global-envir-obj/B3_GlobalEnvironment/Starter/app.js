c96e33e02bcc298ba2029c8b68bb4d8bcf542378
Tahmina <tahmina.ringer@gmail.com>
2021-04-05 21:30:28 -0700

    finished section2

--- starterCode/global-envir-obj/B3_GlobalEnvironment/Starter/app.js
+++ starterCode/global-envir-obj/B3_GlobalEnvironment/Starter/app.js
@@ -33,3 +33,50 @@
 
 // console.log(a);
 
+// Excersize 15
+// function b() {
+//   var myVar;
+//   console.log(myVar);
+// }
+
+// function a() {
+//   var myVar = 2;
+//   console.log(myVar);
+//   b();
+// }
+
+// var myVar = 1;
+// console.log(myVar);
+// a();
+// console.log(myVar);
+
+// Excersize 16
+// function b() {
+//   console.log(myVar);
+// }
+
+// function a() {
+//   var myVar = 2;
+//   b();
+// }
+
+// var myVar = 1;
+// a();
+
+// Excersize 18
+
+function waitThreeSeconds() {
+  var ms = 3000 + new Date().getTime();
+  while(new Date() < ms){}
+  console.log('finished function');
+}
+
+function clickHandler(){
+  console.log('click event!');
+}
+
+//listen for the click event
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
